2ecd39bc7ec5d22b44d21f189a439041f5f9d681
zaki <103634066+ZzakyyNug@users.noreply.github.com>
2024-06-11 22:57:18 +0700

    Delete StudyWithStudent directory

--- StudyWithStudent/Example/src/Main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-  int arr[5] = {1,2,3,4,5};
-  cout << sizeof(arr) / sizeof(arr[0]) << endl;
-}
--- StudyWithStudent/README.md
+++
@@ -1,2 +0,0 @@
-# belajarcpp
-# belajarcpp
